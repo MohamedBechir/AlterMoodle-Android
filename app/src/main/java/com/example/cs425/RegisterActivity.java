97ceb67347917445c7c99e3c7062652dd279961e
MohamedBechir <jamoussi.bechir@yahoo.com>
2020-04-19 18:31:34 +0100

    Add links and 2 register activities

--- app/src/main/java/com/example/cs425/RegisterActivity.java
+++ app/src/main/java/com/example/cs425/RegisterActivity.java
@@ -2,7 +2,12 @@ package com.example.cs425;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +15,27 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+
+        //Create a link for the textview create account
+        TextView textView =(TextView) findViewById(R.id.haveAccount);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        //Create a link for the button Next
+        Button button = (Button) findViewById(R.id.next);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, RegisterFinalActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
