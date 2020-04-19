a3f55e6cc499558fe2c04abceb226486372bd668
MohamedBechir <jamoussi.bechir@yahoo.com>
2020-04-19 23:11:19 +0100

    Add sidebar to home page

--- app/src/main/java/com/example/cs425/MainActivity.java
+++ app/src/main/java/com/example/cs425/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //
         TextView textView = (TextView) findViewById(R.id.register);
         textView.setOnClickListener(new View.OnClickListener() {
            @Override
@@ -20,5 +23,16 @@ public class MainActivity extends AppCompatActivity {
                startActivity(intent);
            }
        });
+
+        //
+        //Create a link for the button Next
+        Button button = (Button) findViewById(R.id.login);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DashboardActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
