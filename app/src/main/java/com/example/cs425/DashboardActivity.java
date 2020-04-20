bf73096b788744d410baa96998d17e04c92b2254
MohamedBechir <jamoussi.bechir@yahoo.com>
2020-04-20 17:48:00 +0100

    Add navbar

--- app/src/main/java/com/example/cs425/DashboardActivity.java
+++ app/src/main/java/com/example/cs425/DashboardActivity.java
@@ -1,14 +1,31 @@
 package com.example.cs425;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
 public class DashboardActivity extends AppCompatActivity {
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, R.string.navigation_drawer_open
+                , R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        super.onBackPressed();
     }
 }
--- app/src/main/java/com/example/cs425/RegisterFinalActivity.java
+++ app/src/main/java/com/example/cs425/RegisterFinalActivity.java
@@ -2,7 +2,10 @@ package com.example.cs425;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterFinalActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class RegisterFinalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_final);
+
+        //Create a link for the button register
+        Button button = (Button) findViewById(R.id.register);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterFinalActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
